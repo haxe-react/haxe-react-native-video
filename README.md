9a7b51dfea392dba53bbafbaa8e04439a7bf883a
Kevin Leung <kevinresol@gmail.com>
2017-01-05 11:56:25 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# haxe-react-native-video
+
+Haxe Extern for react-native-video
---
+++ src/react/native/video/Video.hx
@@ -0,0 +1,4 @@
+package react.native.video;
+
+@:jsRequire('react-native-video', 'default')
+extern class Video extends react.ReactComponent {}
