c89f1832a6147de524184a0821decac577cc4b8f
Kevin Leung <kevinresol@gmail.com>
2017-11-09 01:25:27 +0800

    Add some basic type

--- src/react/native/video/Video.hx
+++ src/react/native/video/Video.hx
@@ -1,4 +1,10 @@
 package react.native.video;
 
+import react.ReactComponent;
+import react.native.component.props.*;
+
 @:jsRequire('react-native-video', 'default')
-extern class Video extends react.ReactComponent {}
+extern class Video extends ReactComponentOfProps<{
+	> ViewProps,
+	source:{uri:String},
+}> {}
